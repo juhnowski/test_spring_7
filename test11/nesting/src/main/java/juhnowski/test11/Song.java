c3bace7eac12bc7ab177fdb95c74067965dda889
juhnowski <juhnowski@gmail.com>
2025-07-09 13:35:31 +0300

    test11 nesting contexts

---
+++ test11/nesting/src/main/java/juhnowski/test11/Song.java
@@ -0,0 +1,13 @@
+package juhnowski.test11;
+
+public class Song {
+    private String title;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
